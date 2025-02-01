57503e118cb9eccd9a4aca86bb4c8714fdad7a16
Jaivir Sohal <js1424@ic.ac.uk>
2025-02-01 21:48:49 +0000

    Added braille to english translator/cleaner using claude

---
+++ example.py
@@ -0,0 +1,5 @@
+from braille_converter import convert_braille_to_english
+
+braille_text = "⠠⠃⠗⠁⠊⠇⠇⠑ ⠑⠭⠁⠍⠏⠇⠑ ⠞⠑⠭⠞"  # Your Braille input
+result = convert_braille_to_english(braille_text)
+print(result)
