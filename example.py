4e50efd070b42034d6bfb09f9be3e40591769c25
Pruthvi Shrikaanth <pruthvishrikaanth@gmail.com>
2025-02-01 22:22:03 +0000

    Uploading web interface

--- example.py
+++
@@ -1,5 +0,0 @@
-from braille_converter import convert_braille_to_english
-
-braille_text = "⠠⠃⠗⠁⠊⠇⠇⠑ ⠑⠭⠁⠍⠏⠇⠑ ⠞⠑⠭⠞"  # Your Braille input
-result = convert_braille_to_english(braille_text)
-print(result)
